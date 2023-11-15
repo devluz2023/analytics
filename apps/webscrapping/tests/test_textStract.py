9b202219026b817bab11dbb4f4d2238cfe1da601
binhojulix <fabio.jdluz@gmail.com>
2023-11-15 17:03:38 -0300

    webscrapping

---
+++ apps/webscrapping/tests/test_textStract.py
@@ -0,0 +1,17 @@
+import pytest
+import urllib.request
+from textStract import TextStract
+
+
+
+@pytest.fixture
+def textStract():
+    return TextStract()
+
+
+def test_decimal(textStract):
+    number = "1.333"
+    result = textStract.convertDouble(number)
+    compare = round(float(number), 2)
+    
+    assert compare == result
